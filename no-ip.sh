bbf28c2b4ea891063646edded4b926c6d1abbece
Steffen Vogel <CodeDevBird@gmail.com>
2018-11-07 20:10:50 +0100

    Refactored script to take advantages of some of jorgempy's changes like remembering last ip. Some other changes.

--- no-ip.sh
+++ no-ip.sh
@@ -38,73 +38,60 @@ then
 	exit 35
 fi
 
-
-SERVICEURL="dynupdate.no-ip.com/nic/update"
-
-case "$SERVICE" in
-        noip)
-            SERVICEURL="dynupdate.no-ip.com/nic/update"
-            ;;
-
-        strato)
-            SERVICEURL="dyndns.strato.com/nic/update"
-            ;;
-         
-        dyndns)
-            SERVICEURL="members.dyndns.org/v3/update"
-            ;;
-         
-        duckdns)
-            SERVICEURL="www.duckdns.org/v3/update"
-            ;;
-        *)
-			SERVICEURL="dynupdate.no-ip.com/nic/update"
- 
-esac
-
 USERAGENT="--user-agent=\"no-ip shell script/1.0 mail@mail.com\""
 BASE64AUTH=$(echo '"$USER:$PASSWORD"' | base64)
 AUTHHEADER="--header=\"Authorization: $BASE64AUTH\""
 
-NOIPURL="https://$USER:$PASSWORD@$SERVICEURL"
-
-
-if [ -n "$IP" ] || [ -n "$HOSTNAME" ]
-then
-	NOIPURL="$NOIPURL?"
-fi
-
-if [ -n "$HOSTNAME" ]
-then
-	NOIPURL="${NOIPURL}hostname=${HOSTNAME}"
-fi
-
-if [ -n "$IP" ]
-then
-	if [ -n "$HOSTNAME" ]
-	then
-		NOIPURL="$NOIPURL&"
-	fi
-	NOIPURL="${NOIPURL}myip=$IP"
-fi
-
-
-echo "$AUTHHEADER $USERAGENT $NOIPURL"
+LASTIP=""
 
 while :
 do
+	if [ -n "$DETECTIP" ]
+	then
+		IP=$(wget -qO- "http://myexternalip.com/raw")
+	fi
 
-	RESULT=$(wget --no-check-certificate -qO- $AUTHHEADER $USERAGENT $NOIPURL)
 
-	 
+	if [ -n "$DETECTIP" ] && [ -z $IP ]
+	then
+		RESULT="Could not detect external IP."
+	else
+		if [ "$LASTIP" != "$IP" ]; then
+			case "$SERVICE" in
+				dynu)
+					RESULT=$(wget --no-check-certificate -S -q https://api.dynu.com/nic/update?hostname=$HOSTNAME\&myip=$IP\&password=$PASSWORD | head -n1)
+					;;
+
+				strato)
+					SERVICEURL="https://dyndns.strato.com/nic/update?hostname=$HOSTNAME\&myip=$IP"
+					echo "Updating ip on: $SERVICEURL"
+
+					RESULT=$(wget --no-check-certificate -S -nv -O "-" --http-user=$USER --http-password=$PASSWORD https://dyndns.strato.com/nic/update?hostname=$HOSTNAME&myip=$IP)
+					;;
+
+				duckdns)
+					RESULT=$(wget --no-check-certificate -qO- $USERAGENT https://www.duckdns.org/update?domains=$HOSTNAME\&token=$USER\&ip=$IP\&verbose=true)
+					;;
+				*)
+					SERVICEURL="dynupdate.no-ip.com/nic/update"
+
+			esac
+		else
+			RESULT="IP unchanged, not updated ($IP)"
+		fi
+	fi
+
+	LASTIP="$IP"
+
+	echo "--- RESULT --------------------"
 	echo $RESULT
-	
+	echo "-------------------------------"
 
 	if [ $INTERVAL -eq 0 ]
 	then
 		break
 	else
-		sleep "${INTERVAL}m" 
+		sleep "${INTERVAL}m"
 	fi
 
 done
